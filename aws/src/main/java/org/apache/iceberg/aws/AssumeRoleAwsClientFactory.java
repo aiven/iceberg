e6c85265beaf26a09f542cc22c52c809f4c077e0
Ryan Skraba <ryan.skraba@aiven.io>
2025-06-18 17:40:58 +0200

    AWS: Add STS endpoint override
    Refactor STS endpoint configuration

--- aws/src/main/java/org/apache/iceberg/aws/AssumeRoleAwsClientFactory.java
+++ aws/src/main/java/org/apache/iceberg/aws/AssumeRoleAwsClientFactory.java
@@ -164,6 +164,7 @@ public class AssumeRoleAwsClientFactory implements AwsClientFactory {
     return StsClient.builder()
         .applyMutation(httpClientProperties::applyHttpClientConfigurations)
         .applyMutation(awsClientProperties::applyClientRegionConfiguration)
+        .applyMutation(awsProperties::applyStsEndpointConfigurations)
         .build();
   }
 
